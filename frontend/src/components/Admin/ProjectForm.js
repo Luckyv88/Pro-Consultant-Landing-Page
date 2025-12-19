08e297c6242456903bd6a8354cbbbabd1b6d744a
Luckyv88 <126507139+Luckyv88@users.noreply.github.com>
2025-12-20 00:19:57 +0530

    Update ProjectForm.js

--- frontend/src/components/Admin/ProjectForm.js
+++ frontend/src/components/Admin/ProjectForm.js
@@ -4,7 +4,7 @@ const ProjectForm = () => {
   const submitHandler = async (e) => {
     e.preventDefault()
     const data = new FormData(e.target)
-    await axios.post('http://localhost:5000/api/projects', data)
+    await axios.post('https://pro-consultant-landing-page-backend.onrender.com/api/projects', data)
     alert('Project Added')
   }
 
